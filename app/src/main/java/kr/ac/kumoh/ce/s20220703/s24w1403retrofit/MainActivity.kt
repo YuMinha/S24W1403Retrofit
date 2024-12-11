f23c88f81f8b3a9a4739289d76e74d298dd5c99f
유민하 <ul8822@kumoh.ac.kr>
2024-12-11 17:39:32 +0900

    🎉 접속 성공

--- app/src/main/java/kr/ac/kumoh/ce/s20220703/s24w1403retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220703/s24w1403retrofit/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kr.ac.kumoh.ce.s20220703.s24w1403retrofit.ui.theme.S24W1403RetrofitTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +23,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             S24W1403RetrofitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(viewModel: SongViewModel = viewModel()) {
+    val songList by viewModel.songList.observeAsState(emptyList())
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        SongResult(
+            list = songList,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    S24W1403RetrofitTheme {
-        Greeting("Android")
+fun SongResult(list: List<Song>, modifier: Modifier) {
+    Column(
+        modifier
+    ) {
+        Text(list.toString())
     }
 }
---
+++ app/src/main/java/kr/ac/kumoh/ce/s20220703/s24w1403retrofit/SongApiConfig.kt
@@ -0,0 +1,6 @@
+package kr.ac.kumoh.ce.s20220703.s24w1403retrofit
+
+object SongApiConfig {
+    const val SERVER_URL = "https://tahsahhuaanhwdpadnvu.supabase.co/rest/v1/"
+    const val API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhaHNhaGh1YWFuaHdkcGFkbnZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzMDEyMzQsImV4cCI6MjA0ODg3NzIzNH0.Wzn6kGh01ATDlSOpWZY34aSupquyp0HXdrKEo0xmAtI"
+}
