98810d7dc39a07b8a134dfbfec162fb147bd711b
유민하 <ul8822@kumoh.ac.kr>
2024-12-11 17:42:20 +0900

    ✨ LazyColumn 사용

--- app/src/main/java/kr/ac/kumoh/ce/s20220703/s24w1403retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220703/s24w1403retrofit/MainActivity.kt
@@ -7,11 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import kr.ac.kumoh.ce.s20220703.s24w1403retrofit.ui.theme.S24W1403RetrofitTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -34,7 +35,7 @@ fun MainScreen(viewModel: SongViewModel = viewModel()) {
     val songList by viewModel.songList.observeAsState(emptyList())
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        SongResult(
+        SongList(
             list = songList,
             modifier = Modifier.padding(innerPadding)
         )
@@ -42,10 +43,12 @@ fun MainScreen(viewModel: SongViewModel = viewModel()) {
 }
 
 @Composable
-fun SongResult(list: List<Song>, modifier: Modifier) {
-    Column(
+fun SongList(list: List<Song>, modifier: Modifier) {
+    LazyColumn(
         modifier
     ) {
-        Text(list.toString())
+        items(list) { song ->
+            Text(song.toString())
+        }
     }
 }
